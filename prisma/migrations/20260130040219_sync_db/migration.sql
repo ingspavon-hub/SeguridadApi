d19b0bc9056dec9b9dcbda264f06768e1314fabb
ingspavon-hub <ing.spavon@gmail.com>
2026-01-29 22:50:33 -0600

    fix: correct syntax and formatting in Prisma configuration and migration files

--- prisma/migrations/20260130040219_sync_db/migration.sql
+++ prisma/migrations/20260130040219_sync_db/migration.sql
@@ -1,14 +1,13 @@
--- This is an empty migration.
-createTable "User" (
-  "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "email"     TEXT NOT NULL UNIQUE,
-  "name"      TEXT,
-  "password"  TEXT NOT NULL,
-  "telephone" TEXT,
-  "createdAt" DATETIME NOT NULL DEFAULT current_timestamp,
-  "updatedAt" DATETIME NOT NULL ,
-    "role"   TEXT    NOT NULL DEFAULT 'USER',
-    "terminalId" INTEGER
+CREATE TABLE "User" (
+  "id"         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "email"      TEXT NOT NULL,
+  "name"       TEXT,
+  "password"   TEXT NOT NULL,
+  "telephone"  TEXT,
+  "createdAt"  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt"  DATETIME NOT NULL,
+  "role"       TEXT NOT NULL DEFAULT 'USER',
+  "terminalId" INTEGER
 );
 
-CREATE UNIQUE INDEX "User.email_KEY" ON "User"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
