eceab41d30c30731059468c856f94089439ab6e2
ingspavon-hub <ing.spavon@gmail.com>
2026-01-30 21:57:07 -0600

    chore: update package dependencies and add dev.db file
    - Added @prisma/adapter-libsql dependency (version ^7.3.0).
    - Downgraded prisma dependency to version ^6.19.2.
    - Created a new database file: dev.db.

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,9 +11,9 @@ import { defineConfig } from '@prisma/config';
 */
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: './prisma/schema.prisma',
   migrations: {
-    path: 'prisma/migrations',
+    path: './prisma/migrations',
   },
   datasource: {
     url: process.env['DATABASE_URL'],
