bfc7425f59d0a43581b2cd31170135e2f2ff600c
ingspavon-hub <ing.spavon@gmail.com>
2026-01-31 23:35:46 -0600

    feat(auth): add Passport module and JwtStrategy to enhance authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule,
     PrismaModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'defaultSecret',
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
